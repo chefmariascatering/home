a1b444eb7055d10b5c751aeb5a52f408919a5167
chefmariascatering <102212565+chefmariascatering@users.noreply.github.com>
2022-04-10 20:59:24 +0800

    Add files via upload

---
+++ scripts/form-carousel_a314.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once('FormProcessor.php');
+
+$form = array(
+    'subject' => 'New Form Submission',
+    'email_message' => 'You have a new form submission',
+    'success_redirect' => '',
+    'sendIpAddress' => true,
+    'email' => array(
+    'from' => '',
+    'to' => 'infochefmariascatering@gmail.com'
+    ),
+    'fields' => array(
+    'name' => array(
+    'order' => 1,
+    'type' => 'string',
+    'label' => 'Name',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Name\' is required.'
+    )
+    ),
+    'email' => array(
+    'order' => 2,
+    'type' => 'email',
+    'label' => 'Email',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Email\' is required.'
+    )
+    ),
+    'textarea' => array(
+    'order' => 3,
+    'type' => 'string',
+    'label' => 'Your Message',
+    'required' => false,
+    'errors' => array(
+    'required' => 'Field \'Your Message\' is required.'
+    )
+    ),
+    )
+    );
+
+    $processor = new FormProcessor('');
+    $processor->process($form);
+
+    ?>
